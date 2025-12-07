14895e91d4053d86131805d842fd1c7a9a206ea4
xotu6066 <aymericcasteret@orange.fr>
2025-12-07 18:20:57 +0100

    Update Pertes.c

--- Pertes.c
+++ Pertes.c
@@ -1,1 +1,9 @@
-
+int perteUsine(arbre usine ){
+  if (usine==NULL){
+    return -1
+  }
+  else{
+    return som_perte(usine) ; // faut faire une fonction qui additionne toute les pertes presente dans tous les sous arbe comme pour la fonction prefix mais avec des arbre qui n'ont pas de limite de nombre de sous arbre et sa renvoie un int 
+  }
+}
+// je sais pas si c'est comme ca qu'il faut faire
