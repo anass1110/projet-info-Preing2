8fcc0c0c05f11f714774bcd9d52eae0aa0818977
xotu6066 <aymericcasteret@orange.fr>
2025-12-15 09:51:56 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -28,8 +28,65 @@ if [ ! -f "$chemin" ]; then
   erreur "Fichier d'entrée '$chemin' introuvable."
 fi
 
+if [ "$mode" = "histo" ]; then
 
+    if [ $# -ne 3 ]; then
+        erreur "histo nécessite max ou src | real."
+    fi
+case "$option" in
+        max)
+            echo "identifier / max volume (k.m3.year-1)"
+            while IFS=';' read -r id max src real; do
+                echo "$id / $max"
+            done < "$chemin"
+            ;;
+        src)
+            echo "identifier / source volume (k.m3.year-1)"
+            while IFS=';' read -r id max src real; do
+                echo "$id / $src"
+            done < "$chemin"
+            ;;
+        real)
+            echo "identifier / real volume (k.m3.year-1)"
+            while IFS=';' read -r id max src real; do
+                echo "$id / $real"
+            done < "$chemin"
+            ;;
+        *)
+            erreur "Argument histo invalide."
+            ;;
+    esac
 
+    exit 0
+fi
+
+if [ "$MODE" = "leaks" ]; then
+
+    if [ $# -ne 3 ]; then
+        erreur "leaks nécessite un identifiant."
+    fi
+
+    f=0
+    som=0
+
+    echo "identifier / Leak volume (M.m3.year-1)"
+
+    while IFS=';' read -r id max src real; do
+        if [ "$id" = "$option" ]; then
+            leak=$((src - real))
+            som=$((som + leak))
+            f=1
+        fi
+    done < "$chemin"
+
+    if [ $f -eq 1 ]; then
+        echo "$option / $som"
+    else
+        echo "$option / -1"
+    fi
+
+    exit 0
+fi
 
 
 
