cc15256e2a457ad57e3f55e2d04a9dbff59736d2
anass1110 <anass2006@icloud.com>
2025-12-19 09:59:29 +0100

    Update arbre.h

--- arbre.h
+++ arbre.h
@@ -1,1 +1,22 @@
+#ifndef ARBRE_H
+#define ARBRE_H
 
+typedef struct NoeudReseau NoeudReseau;
+
+typedef struct Enfant {
+    NoeudReseau *noeud;
+    double fuite;            /* en pourcentage */
+    struct Enfant *suiv;
+} Enfant;
+
+struct NoeudReseau {
+    char identifiant[256];
+    Enfant *enfants;
+};
+
+NoeudReseau* creer_noeud(const char *identifiant);
+int ajouter_enfant(NoeudReseau *parent, NoeudReseau *enfant, double fuite);
+int compter_enfants(NoeudReseau *noeud);
+void liberer_noeud(NoeudReseau *noeud);
+
+#endif
