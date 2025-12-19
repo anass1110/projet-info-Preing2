e5a678cd3609b70734bc8dacf0396cc8ffc4faee
anass1110 <anass2006@icloud.com>
2025-12-19 10:00:19 +0100

    Update Utilitaire.h

--- Utilitaire.h
+++ Utilitaire.h
@@ -1,1 +1,13 @@
+#ifndef UTILITAIRE_H
+#define UTILITAIRE_H
+
+#include <stdio.h>
+
+/* Test de préfixe */
+int commence_par(const char *chaine, const char *prefixe);
+
+/* Message d’erreur */
+void erreur(const char *message);
+
+#endif
 
