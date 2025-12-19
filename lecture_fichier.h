ccafffc7a0d46cd48a7fce42cb8c20a4a6d89533
anass1110 <anass2006@icloud.com>
2025-12-19 09:53:26 +0100

    Update lecture_fichier.h

--- lecture_fichier.h
+++ lecture_fichier.h
@@ -1,1 +1,26 @@
+#ifndef LECTURE_FICHIER_H
+#define LECTURE_FICHIER_H
 
+#include <stdio.h>
+
+#define TAILLE_CHAMP 256
+#define TAILLE_LIGNE 1024
+
+typedef struct {
+    char col1[TAILLE_CHAMP];
+    char col2[TAILLE_CHAMP];
+    char col3[TAILLE_CHAMP];
+    char col4[TAILLE_CHAMP];
+    char col5[TAILLE_CHAMP];
+} LigneCSV;
+
+/* Lit une ligne CSV  */
+int lire_ligne_csv(FILE *fichier, LigneCSV *ligne);
+
+/* Tests pour les champs */
+int champ_est_vide_ou_tiret(const char *chaine);
+
+/* Conversion pour sécurisée */
+double lire_double_ou_0(const char *chaine);
+
+#endif
