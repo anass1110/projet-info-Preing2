91401459d944d6f70740461307b8f70c5c32cb03
anass1110 <anass2006@icloud.com>
2025-12-19 09:56:28 +0100

    Update pertes.h

--- pertes.h
+++ pertes.h
@@ -1,1 +1,14 @@
+#ifndef PERTES_H
+#define PERTES_H
+
+typedef struct {
+    char amont[256];
+    char aval[256];
+    double perte_max;
+} TronconMax;
+
+/* Calcule les pertes d'eau en aval d'une usine */
+int calculer_leaks(const char *csv_path,const char *identifiant_usine,const char *chemin_sortie);
+
+#endif 
 
