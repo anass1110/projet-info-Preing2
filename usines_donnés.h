942e6fa2179f7a50358f9ac48b3494b9d37bd8d3
anass1110 <anass2006@icloud.com>
2025-12-19 09:58:12 +0100

    Update usines_donnés.h

--- usines_donnés.h
+++ usines_donnés.h
@@ -1,1 +1,8 @@
+#ifndef USINES_DONNES_H
+#define USINES_DONNES_H
+
+/* Génère un fichier histogramme pour les usines */
+int generer_histo(const char *chemin_csv,const char *mode,const char *chemin_sortie);
+
+#endif
 
